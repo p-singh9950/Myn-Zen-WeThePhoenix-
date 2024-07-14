09b269628765d5c7a3b1a4ce816b3d20167e6723
p-singh9950 <139635900+p-singh9950@users.noreply.github.com>
2024-07-14 14:55:38 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDoODR8ytrB5FzW3_4K3DPbpiRBSs4UxM8",
-  authDomain: "myntra-mynzen.firebaseapp.com",
-  projectId: "myntra-mynzen",
-  storageBucket: "myntra-mynzen.appspot.com",
-  messagingSenderId: "1011440183969",
-  appId: "1:1011440183969:web:254e107402116167f19ee0",
-  measurementId: "G-SV6DZQ1G4E"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
